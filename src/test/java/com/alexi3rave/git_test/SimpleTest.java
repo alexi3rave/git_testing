94b6a1b3eb6433de968e54b6f5535bf494b64a02
alexi3rave <144437542+alexi3rave@users.noreply.github.com>
2023-10-13 15:10:53 +0300

    added simple test

---
+++ src/test/java/com/alexi3rave/git_test/SimpleTest.java
@@ -0,0 +1,11 @@
+package com.alexi3rave.git_test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+    @Test
+    void simpleTest {
+        Assertions.assertTrue(true);
+    }
+}
